99948dcc2af5178dc240a5fe82c1e2e0bef38c9c
lixulei <lixl01@asp.citic.com>
2018-11-21 15:48:27 +0800

    提交文件

---
+++ TSL-om/src/components/discountcodeTable.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="cpTableCm">
+    <div>
+      <table style="border-collapse: collapse">
+        <tr>
+          <td width="150" style="text-align: center;border: 1px solid black;">折扣码</td>
+          <td width="150" style="text-align: center;border: 1px solid black;">系统员工编号</td>
+          <td width="150" style="text-align: center;border: 1px solid black;">状态</td>
+          <td width="150" style="text-align: center;border: 1px solid black;">可使用次数</td>
+          <td width="150" style="text-align: center;border: 1px solid black;">兑换时间</td>
+          <td width="150" style="text-align: center;border: 1px solid black;">兑换人手机号码</td>
+          <td width="150" style="text-align: center;border: 1px solid black;">兑换人账号</td>
+          <td width="150" style="text-align: center;border: 1px solid black;">使用时间</td>
+          <td width="150" style="text-align: center;border: 1px solid black;">订单编号</td>
+          <td width="150" style="text-align: center;border: 1px solid black;">订单金额</td>
+        </tr>
+        <tr v-for="list in dataList">
+          <td width="150" style="text-align: center;border: 1px solid black;">{{list.discountCode}}</td>
+          <td width="150" style="text-align: center;border: 1px solid black;">{{list.operator}}</td>
+          <td width="150" style="text-align: center;border: 1px solid black;">{{list.statusName}}</td>
+          <td width="150" style="text-align: center;border: 1px solid black;">{{list.remainTimes}}</td>
+          <td width="150" style="text-align: center;border: 1px solid black;">{{list.bindDate}}</td>
+          <td width="150" style="text-align: center;border: 1px solid black;">{{list.telePhone}}</td>
+          <td width="150" style="text-align: center;border: 1px solid black;">{{list.consumer}}</td>
+          <td width="150" style="text-align: center;border: 1px solid black;">{{list.costTime}}</td>
+          <td width="150" style="text-align: center;border: 1px solid black;">{{list.consumeOrder}}</td>
+          <td width="150" style="text-align: center;border: 1px solid black;">{{list.orderMoney}}</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  export default {
+    name: 'cpTableCm',
+    data () {
+      return {
+
+      }
+    },
+    props: {
+      dataList: [Array]
+    }
+  }
+</script>
